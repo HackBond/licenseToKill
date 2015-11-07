798b586fc97ed1a6ad7e3747e0b745e5b5ad62ea
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2015-11-07 13:42:48 +0000

    add welcome

--- 2kill/app.js
+++ 2kill/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var welcome = require('./routes/welcome');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login', login);
+app.use('/welcome', welcome);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- 2kill/routes/login.js
+++ 2kill/routes/login.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req, res, next) {
-
+    res.redirect('/welcome?name=JamesBond');
 });
 
 
