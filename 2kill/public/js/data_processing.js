3f92d123b00400ef01503c3743ba17415ebf4214
ArioA <asa215@ic.ac.uk>
2015-11-07 18:48:22 +0000

    Added comments to all the functions. Added rate_of_change() which gives the rate of change of the agent's heart rate from the last n data points. Modified process_last_ten() function to process_last_n() for flexibility. Added sampler() and monitor() functions which print the incomming data in a steady stream (badly).

--- 2kill/public/js/data_processing.js
+++ 2kill/public/js/data_processing.js
@@ -1,3 +1,13 @@
+//Ario Aliabadi
+//asa215@ic.ac.uk
+
+//Some functions to analyse the data coming in form the biometrics on
+//the agent's person.
+
+
+//Random array of numbers which supposedly simmulate the heart rate 
+//of the agent.
+
 var rand_heartrate = new Array(1000);
 
 for (var i = rand_heartrate.length - 1; i >= 0; i--) {
@@ -5,6 +15,7 @@ for (var i = rand_heartrate.length - 1; i >= 0; i--) {
 };
 
 
+//Calculates the average of the elements of an array.
 function heart_rate_avg(last_ten_heartbeats){
 
 	var sum = 0;
@@ -18,6 +29,8 @@ function heart_rate_avg(last_ten_heartbeats){
 	return sum;
 }
 
+
+//Outputs the current status of the agent bassed on their heart rate.
 function heart_rate_response(last_ten_avg){
 
 	if(last_ten_avg == 0)
@@ -30,20 +43,58 @@ function heart_rate_response(last_ten_avg){
 		return "Poisoned";
 }
 
-function process_last_ten(long_array){
+//Calculates the average of the last n data points in long_array and
+//returns the appropriate message from heart_rate_response().
+function process_last_n(long_array, n){
 
-	var x = new Array(10);
+	var last_n = new Array(n);
 
-
-	for (var i =  long_array.length-1; i > long_array.length - 11; i--) {
-		x[long_array.length - i - 1] = long_array[i];
+	for (var i =  long_array.length-1; i > long_array.length - 1 - n; i--) {
+		last_n[long_array.length - i - 1] = long_array[i];
 	};
 
-	return heart_rate_response(heart_rate_avg(x));
+	return heart_rate_response(heart_rate_avg(last_n));
 }
 
-console.log(heart_rate_avg(rand_heartrate));
-console.log(process_last_ten(rand_heartrate));
+
+//Calculates the rate of change between the latest and the nth last 
+//data point in long_array.
+function rate_of_change(long_array, n){
+
+	var last_n = new Array(n);
+
+	for (var i = long_array.length - 1; i > long_array.length - n - 1; i--) {
+		last_n[long_array.length - 1 - i] = long_array[i];	
+	};
+
+	return (last_n[n-1]/last_n[0] - 1)*100;
+}
+
+
+//Prints the status of the agent in a trickle down basis.
+function monitor(){
+
+for(var k = 0; k < 2000000000; k += 1)
+	{
+		if(k % 200000000 == 0)
+		sampler();
+	}
+
+	
+}
+
+
+
+function sampler () {
+	rand_heartrate.push(Math.random()*20 + 100);
+	console.log(process_last_ten(rand_heartrate));
+}
+
+monitor();
+
+
+//console.log(heart_rate_avg(rand_heartrate));
+//console.log(process_last_ten(rand_heartrate));
 
 /*
 function monitor() {
